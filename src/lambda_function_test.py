fc38a6207e2e52d91607bff16b988b4f96c4b1c5
Floris Van den Abeele <floris@vdna.be>
2023-02-23 11:39:34 +0100

    test: add short test function

---
+++ src/lambda_function_test.py
@@ -0,0 +1,15 @@
+import datetime
+
+import lambda_function
+
+def test_get_cyclemap_entries():
+    bbox = lambda_function.BBox(-180., -90., 180., 90.)
+    begin = datetime.datetime(2017, 7, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
+    end = datetime.datetime(2022, 5, 24, 0, 0, 0, tzinfo=datetime.timezone.utc)
+    entries = lambda_function.get_cyclemap_entries(bbox, begin ,end)
+
+    assert len(entries) > 0
+    print('test_get_cyclemap_entries() succes')
+
+if __name__ == '__main__':
+    test_get_cyclemap_entries()
