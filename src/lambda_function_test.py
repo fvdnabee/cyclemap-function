fceb54912867ba0d232efeb67e6ed6f0725d4b24
Floris Van den Abeele <floris@vdna.be>
2024-05-24 14:03:11 +0200

    test: check entry dict keys

--- src/lambda_function_test.py
+++ src/lambda_function_test.py
@@ -9,6 +9,9 @@ def test_get_cyclemap_entries():
     entries = lambda_function.get_cyclemap_entries(bbox, begin ,end)
 
     assert len(entries) > 0
+    entry = entries[0]
+    assert {'created_at', 'url', 'content', 'media_attachments', 'account', 'location'} <= set(entry.keys())
+
     print('test_get_cyclemap_entries() succes')
 
 if __name__ == '__main__':
