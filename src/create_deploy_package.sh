c0ae2d6f7744c3d511b93569fa2619c028420569
Floris Van den Abeele <floris@vdna.be>
2024-05-24 14:09:43 +0200

    build: create deploy package with py3.12

--- src/create_deploy_package.sh
+++ src/create_deploy_package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 # https://docs.aws.amazon.com/lambda/latest/dg/python-package.html#python-package-create-package-with-dependency
 rm -rf package
-python3.9 -m pip install --target ./package -r requirements.txt
+python3.12 -m pip install --target ./package -r requirements.txt
 
 (cd package && zip -r - .) > deployment_package.zip
 zip -g deployment_package.zip lambda_function.py
