b03507b3c1bd3aeb0f2e910a321bb276f0689548
Floris Van den Abeele <floris@vdna.be>
2024-05-24 14:25:53 +0200

    chore: release lambda function 0.0.2

--- tf/main.tf
+++ tf/main.tf
@@ -75,10 +75,14 @@ EOF
 #   # }
 # }
 
+locals {
+  deployment_package = "../src/deployment_package-0.0.2.zip"
+}
+
 resource "aws_lambda_function" "cyclemap" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
-  filename      = "../src/deployment_package.zip"
+  filename      = local.deployment_package
   function_name = "cyclemap_entries"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
@@ -86,7 +90,7 @@ resource "aws_lambda_function" "cyclemap" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("../src/deployment_package.zip")
+  source_code_hash = filebase64sha256(local.deployment_package)
 
   architectures = ["arm64"]
   runtime       = "python3.12"
